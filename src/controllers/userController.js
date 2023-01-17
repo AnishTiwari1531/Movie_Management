c9572dacbdcb00e5ee8b774327f282c4e2e17cc4
AnishTiwari1531 <anishtiwari1531@gmail.com>
2023-01-17 21:34:24 +0530

    Done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,190 +1,194 @@
 const nodemailer = require("nodemailer");
-const sgMail = require('@sendgrid/mail'); 
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const userModel = require("../models/userModel");
 const otpModel = require("../models/otpModel")
-const {isValidName,isValidEmail,isValidRequest,isValidValue} = require("../utils/validator");
+const { isValidName, isValidEmail, isValidRequest, isValidValue } = require("../utils/validator");
 const saltRounds = 10;
 let triesLeft;
 
 //----------------------------------USER-Sign Up --------------------------------//
 
 const register = async (req, res) => {
-    try {
-      const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-      //Request Validation
-      if (!isValidRequest(req.body)) {
-        return res.status(400).send({ Status:'Failed', Message:"Please fill the details" });
-      }
+    //Request Validation
+    if (!isValidRequest(req.body)) {
+      return res.status(400).send({ Status: 'Failed', Message: "Please fill the details" });
+    }
 
-      //Name Validation
+    //Name Validation
+    if (!isValidValue(name)) {
+      return res.status(400).send({ Status: 'Failed', Message: "Please enter your Name" });
+    }
+    if (!isValidName(name)) {
+      return res.status(400).send({ Status: 'Failed', Message: "Enter your name properly" });
+    }
+
+    //Email Validation & Duplicate Checking
+    if (!isValidValue(email)) {
+      return res.status(400).send({ Status: 'Failed', Message: "Please enter your email" });
+    }
+    if (!isValidEmail(email)) {
+      return res.status(400).send({ Status: 'Failed', Message: "Email is not valid" });
+    }
+
+    const emailExist = await userModel.findOne({ email });
+    if (emailExist) {
+      return res.status(400).send({ Status: 'Failed', Message: "This email already exists" });
+    }
+
+    //Password Validation
+    if (!password || password.length < 8 || password.length > 15) {
+      return res.status(400).send({
+        Status: 'Failed',
+        Message: "Password length should be between 8 to 15"
+      });
+    }
+    if (req.body.password.trim().length !== req.body.password.length) {
+      return res.status(400).send({ Status: 'Failed', Message: "Space not allowed in Password" });
+    }
+
+    req.body.password = await bcrypt.hash(password, saltRounds);    //Password Hashing
+    let newUser = await userModel.create(req.body);
+
+    newUser = newUser.toObject();
+    delete newUser.password;
+
+    return res.status(201).send({ Status: 'Success', 'User Details': newUser });
+  }
+  catch (error) {
+    res.status(500).send({ Status: 'Failed', Message: error.message });
+  }
+};
+
+
+
+//----------------------------------USER-Sign In --------------------------------//
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //Request Validation
+    if (!isValidRequest(req.body)) {
+      return res.status(400).send({ Status: 'Failed', Message: "Enter email & password" });
+    }
+
+    //Email Validation
+    if (!isValidValue(email)) {
+      return res.status(400).send({ Status: 'Failed', Messgage: "Enter your Email" });
+    }
+
+    const checkUser = await userModel.findOne({ email });
+    if (!checkUser) {
+      return res.status(404).send({ Status: 'Failed', Message: "Email not found" });
+    }
+
+    //Password Validation
+    if (!isValidValue(password)) {
+      return res.status(400).send({ Status: 'Failed', Messsge: "Enter Password to login" });
+    }
+
+    let decryptPassword = await bcrypt.compare(password, checkUser.password);
+    if (!decryptPassword) {
+      return res.status(401).send({ Status: 'Failed', Message: "Password is not correct" });
+    }
+
+
+    //TOKEN CREATION
+
+    const currTimeStamp = Date.now();
+    const createTime = Math.floor(currTimeStamp / 1000);
+    const expTime = createTime + (12 * 60 * 60);
+
+    const token = jwt.sign(
+      {
+        userId: checkUser._id.toString(),
+        iat: createTime,
+        exp: expTime,
+      },
+      process.env.JWT_TOKEN
+    );
+
+    res.setHeader("x-api-key", token);
+    return res.status(200).send({ Status: "Success", Token: token });
+  }
+  catch (error) {
+    res.status(500).send({ Status: 'Failed', Message: error.message });
+  }
+};
+
+
+
+
+//----------------------------------USER-Profile Update --------------------------------//
+
+const updateUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //Request Validation
+    if (!isValidRequest(req.body)) {
+      return res.status(400).send({ Status: 'Failed', Message: "Nothing to update" });
+    }
+
+    //Name Validation
+    if (Object.keys(req.body).includes('name')) {
       if (!isValidValue(name)) {
-        return res.status(400).send({ Status:'Failed', Message:"Please enter your Name" });
+        return res.status(400).send({ Status: 'Failed', Message: "Please enter your Name" });
       }
       if (!isValidName(name)) {
-        return res.status(400).send({ Status:'Failed', Message:"Enter your name properly" });
+        return res.status(400).send({ Status: 'Failed', Message: "Enter your name properly" });
       }
+    }
 
-      //Email Validation & Duplicate Checking
+    //Email Validation
+    if (Object.keys(req.body).includes('email')) {
       if (!isValidValue(email)) {
-        return res.status(400).send({ Status:'Failed', Message:"Please enter your email"});
+        return res.status(400).send({ Status: 'Failed', Message: "Please enter your email" });
       }
       if (!isValidEmail(email)) {
-        return res.status(400).send({ Status:'Failed', Message:"Email is not valid"});
+        return res.status(400).send({ Status: 'Failed', Message: "Email is not valid" });
       }
-
-      const emailExist = await userModel.findOne({ email });
+      let emailExist = await userModel.findOne({ email });
       if (emailExist) {
         return res.status(400).send({ Status: 'Failed', Message: "This email already exists" });
       }
-
-      //Password Validation
-      if (!password || password.length < 8 || password.length > 15){
-        return res.status(400).send({ Status:'Failed', 
-        Message:"Password length should be between 8 to 15" });
-      }
-      if(req.body.password.trim().length!==req.body.password.length){
-        return res.status(400).send({ Status:'Failed', Message:"Space not allowed in Password" });
-      }
-      
-      req.body.password = await bcrypt.hash(password, saltRounds);    //Password Hashing
-      let newUser = await userModel.create(req.body);
-
-      newUser=newUser.toObject();
-      delete newUser.password;
-
-      return res.status(201).send({ Status: 'Success', 'User Details': newUser });
-    } 
-    catch (error) {
-      res.status(500).send({ Status: 'Failed', Message: error.message });
     }
-  };
-  
 
-
-  //----------------------------------USER-Sign In --------------------------------//
-  
-  const login = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-
-      //Request Validation
-      if (!isValidRequest(req.body)) {
-      return res.status(400).send({ Status: 'Failed', Message:"Enter email & password" });
+    //Password Validation
+    if (Object.keys(req.body).includes('password')) {
+      if (password.length < 8 || password.length > 15) {
+        return res.status(400).send({
+          Status: 'Failed',
+          Message: "Password length should be between 8 to 15"
+        });
       }
 
-      //Email Validation
-      if (!isValidValue(email)){ 
-      return res.status(400).send({ Status: 'Failed', Messgage: "Enter your Email" });
+      if (req.body.password.trim().length !== req.body.password.length) {
+        return res.status(400).send({ Status: 'Failed', Message: "Space not allowed in Password" });
       }
-      
-      const checkUser = await userModel.findOne({ email });
-      if (!checkUser) {
-        return res.status(404).send({ Status:'Failed', Message: "Email not found"});
-      }
-      
-      //Password Validation
-      if (!isValidValue(password)) {
-        return res.status(400).send({ Status: 'Failed', Messsge: "Enter Password to login"});
-      }
-  
-      let decryptPassword = await bcrypt.compare(password, checkUser.password);
-      if (!decryptPassword) {
-        return res.status(401).send({ Status: 'Failed', Message: "Password is not correct" });
-      } 
-     
 
-      //TOKEN CREATION
-
-      const currTimeStamp = Date.now();
-      const createTime = Math.floor(currTimeStamp / 1000);
-      const expTime = createTime + (12*60*60);
-  
-      const token = jwt.sign(
-        {
-          userId: checkUser._id.toString(),
-          iat: createTime,
-          exp: expTime,
-        },
-        process.env.JWT_TOKEN
-      );
-  
-      res.setHeader("x-api-key", token);
-      return res.status(200).send({ Status: "Success", Token: token });
-    } 
-    catch (error) {
-      res.status(500).send({ Status: 'Failed', Message: error.message });
+      //Hashing Password
+      req.body.password = await bcrypt.hash(password, saltRounds);
     }
-  };
-  
 
-
-
-  //----------------------------------USER-Profile Update --------------------------------//
-
-const updateUser = async (req,res) => {
-    try {
-      const { name, email, password } = req.body;
-
-      //Request Validation
-      if (!isValidRequest(req.body)) {
-        return res.status(400).send({ Status:'Failed', Message:"Nothing to update" });
-      }
-      
-      //Name Validation
-      if(Object.keys(req.body).includes('name')){
-        if (!isValidValue(name)) {
-          return res.status(400).send({ Status:'Failed', Message:"Please enter your Name" });
-        }
-        if (!isValidName(name)) {
-          return res.status(400).send({ Status:'Failed', Message:"Enter your name properly" });
-        }
-      }
-
-      //Email Validation
-      if(Object.keys(req.body).includes('email')){
-        if (!isValidValue(email)) {
-          return res.status(400).send({ Status:'Failed', Message:"Please enter your email"});
-        }
-        if (!isValidEmail(email)) {
-          return res.status(400).send({ Status:'Failed', Message:"Email is not valid"});
-        }
-        let emailExist = await userModel.findOne({ email });
-        if (emailExist) {
-          return res.status(400).send({ Status: 'Failed', Message: "This email already exists" });
-        }
-      }
-
-      //Password Validation
-      if(Object.keys(req.body).includes('password')) {
-        if (password.length < 8 || password.length > 15) {
-          return res.status(400).send({ Status:'Failed', 
-            Message:"Password length should be between 8 to 15" });
-        }
-
-        if(req.body.password.trim().length!==req.body.password.length) {
-          return res.status(400).send({ Status:'Failed', Message:"Space not allowed in Password"});
-        }
-        
-        //Hashing Password
-        req.body.password = await bcrypt.hash(password, saltRounds);  
-      }
-
-      await userModel.findByIdAndUpdate({ _id: req.userId }, req.body, { new: true });
-      return res.status(200).send({ Status: "Success", "Message":'User Profile Updated' });
-    } 
-    catch (error) {
-      res.status(500).send({ Status: 'Failed', Message: error.message });
-    }
+    await userModel.findByIdAndUpdate({ _id: req.userId }, req.body, { new: true });
+    return res.status(200).send({ Status: "Success", "Message": 'User Profile Updated' });
+  }
+  catch (error) {
+    res.status(500).send({ Status: 'Failed', Message: error.message });
+  }
 }
 
 
 
-  //----------------------------------USER-Profile Delete --------------------------------//
+//----------------------------------USER-Profile Delete --------------------------------//
 
 const deleteUser = async (req, res) => {
   try {
@@ -202,58 +206,74 @@ const deleteUser = async (req, res) => {
 
     //Authorization Check
     if (req.userId !== userData._id.toString()) {
-      return res.status(403).send({Status: "Failed",
-          Message: "You're not allowed to delete this account",});
+      return res.status(403).send({
+        Status: "Failed",
+        Message: "You're not allowed to delete this account",
+      });
     }
 
     //Permanently deleting User account. Ideally we use a flag isDeleted to just hide the account.
     await userModel.findByIdAndRemove({ _id: userId });
     return res.status(204).send({ Status: "Success" });
-  } 
+  }
   catch (error) {
     res.status(500).send({ Status: "Failed", Message: error.message });
   }
 };
 
 
-    //----------------------------------Forgot Password--------------------------------//
+//----------------------------------Forgot Password--------------------------------//
 
 //USING NODE MAILER
-const forgotPassword = async (req,res) => {
-  try{
-  let testAccount = await nodemailer.createTestAccount();
-  const email = req.body.email ;
-  triesLeft = 3;
+const forgotPassword = async (req, res) => {
+  try {
+    //let testAccount = await nodemailer.createTestAccount();
+    const email = req.body.email;
+    triesLeft = 3;
 
-  let userRecord = await userModel.findOne({email});
-  if(!userRecord) {
-    return res.status(404).send({ Status: "Failed", Message: "User Doesn't exists" });
-  }
-  const otp = Math.random().toString().substring(2, 8);
-  //Math.floor(100000 + Math.random() * 900000);
-
-  // create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'nova.bogisich83@ethereal.email',
-        pass: 'ShSg1ygNrXKQfUHtgk'
+    let userRecord = await userModel.findOne({ email });
+    if (!userRecord) {
+      return res.status(404).send({ Status: "Failed", Message: "User Doesn't exists" });
     }
-  });
+    const otp = Math.random().toString().substring(2, 8);
+    //Math.floor(100000 + Math.random() * 900000);
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: email, // list of receivers
-    subject: "Verification Password", // Subject line
-    text: "Your One Time Password is  "+otp, // plain text body
-   // html: "<b>Verification Password</b>", // html body
-  });
+    // create reusable transporter object using the default SMTP transport
+    // const transporter = nodemailer.createTransport({
+    //   host: 'smtp.ethereal.email',
+    //   port: 587,
+    //   auth: {
+    //       user: 'nova.bogisich83@ethereal.email',
+    //       pass: 'ShSg1ygNrXKQfUHtgk'
+    //   }
+    // });
 
-  await otpModel.create({ email, otp });
 
-  return res.status(200).send(`Message sent: ${info.messageId}, Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
+    //Gmail....https://myaccount.google.com/ >>> Security >>> App PAssword >>>Generate new pswd
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      //port: 465,
+      secure: true,
+      auth: {
+        user: 'anishtiwariatwork@gmail.com',
+        pass: 'cnjjengefnxlaetn'
+      }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"ANISH TIWARI👻" <anishtiwariatwork@gmail.com>', // sender address
+      to: email, // list of receivers
+      subject: "Verification Password", // Subject line
+      text: "Your One Time Password is  " + otp, // plain text body
+      // html: "<b>Verification Password</b>", // html body
+    });
+
+    await otpModel.create({ email, otp });
+
+    return res.status(200).send(`Message sent Successfully... 
+          From : ${info.envelope.from} To : ${info.envelope.to} `);
   }
   catch (error) {
     res.status(500).send({ Status: "Failed", Message: error.message });
@@ -285,41 +305,39 @@ const forgotPassword = async (req,res) => {
 
 
 
-    //----------------------------------Verify OTP --------------------------------//
+//----------------------------------Verify OTP --------------------------------//
 
 
-const verifyOtp = async (req,res) => {
-    try{
-      
-      const { email, otp, password } = req.body;
-      const fiveMinutesAgo = new Date(Date.now() - 1000 * 60 * 5);
-      const otpData = await otpModel.findOne({ email, otp, timestamp: { $gte: fiveMinutesAgo } });
-      
-      if(triesLeft==0){
-        await otpModel.deleteMany();
-        return res.status(400).send({Status:"Failed", Message: 'You\'ve reached maximum tries' });
-      }
+const verifyOtp = async (req, res) => {
+  try {
 
-      if (otpData) {
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        await userModel.updateOne({ email }, { password: hashedPassword });
-        await otpModel.deleteOne({otp});
-        
-        res.status(200).send({Status:"Success", Message: 'Password reset successful. Please login again with your new password' });
-      } 
-      else {
-          triesLeft--;
-          res.status(400).send({Status:"Failed", Message: `Invalid OTP,Tries Left:${triesLeft}` });
-      }
+    const { email, otp, password } = req.body;
+    const fiveMinutesAgo = new Date(Date.now() - 1000 * 60 * 5);
+    const otpData = await otpModel.findOne({ email, otp, timestamp: { $gte: fiveMinutesAgo } });
+
+    if (triesLeft == 0) {
+      await otpModel.deleteMany();
+      return res.status(400).send({ Status: "Failed", Message: 'You\'ve reached maximum tries' });
     }
-    catch (error) {
-      res.status(500).send({ Status: "Failed", Message: error.message });
+
+    if (otpData) {
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      await userModel.updateOne({ email }, { password: hashedPassword });
+      await otpModel.deleteOne({ otp });
+
+      res.status(200).send({ Status: "Success", Message: 'Password reset successful. Please login again with your new password' });
     }
+    else {
+      triesLeft--;
+      res.status(400).send({ Status: "Failed", Message: `Invalid OTP,Tries Left:${triesLeft}` });
+    }
+  }
+  catch (error) {
+    res.status(500).send({ Status: "Failed", Message: error.message });
+  }
 }
 
 
 
 
-  module.exports= {register, login, updateUser, deleteUser, forgotPassword, verifyOtp};
-  
-  
+module.exports = { register, login, updateUser, deleteUser, forgotPassword, verifyOtp };
